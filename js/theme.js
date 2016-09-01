aa2700abb3e3f35a0d2c039d30bae53f2b8330d1
Frost <cpaccione77@gmail.com>
2016-09-01 17:54:25 -0500

    Modal updates

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,75 @@
 jQuery(document).ready(function($) {
 
+	// Init card flip function
 	$(".card").flip({
 		trigger: "hover"
 	});
 
-	// $(document).foundation();
+	// Stop YouTube video playing when modal is switched or closed
+	$("#modal-one, #modal-two").on('hidden.bs.modal', function (e) {
+	    $("#modal-one iframe, #modal-two iframe").attr("src", $("#modal-one iframe, #modal-two iframe").attr("src"));
+	});
+
+	// $(function() {
+
+	// 	var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+	// 	$fluidEl = $("figure");
+
+	// 		$allVideos.each(function() {
+
+	// 			$(this)
+	// 			// jQuery .data does not work on object/embed elements
+	// 			.attr('data-aspectRatio', this.height / this.width)
+	// 			.removeAttr('height')
+	// 			.removeAttr('width');
+
+	// 		});
+
+	// 		$(window).resize(function() {
+
+	// 			var newWidth = $fluidEl.width();
+	// 			$allVideos.each(function() {
+
+	// 				var $el = $(this);
+	// 				$el
+
+	// 					.width(newWidth)
+	// 					.height(newWidth * $el.attr('data-aspectRatio'));
+
+	// 			});
+
+	// 		}).resize();
+
+
+	$(function() {
+
+    var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+    $fluidEl = $("figure");
+
+	$allVideos.each(function() {
+
+	  $(this)
+	    // jQuery .data does not work on object/embed elements
+	    .attr('data-aspectRatio', this.height / this.width)
+	    .removeAttr('height')
+	    .removeAttr('width');
+
+	});
+
+	$(window).resize(function() {
+
+	  var newWidth = $fluidEl.width();
+	  $allVideos.each(function() {
+
+	    var $el = $(this);
+	    $el
+	        .width(newWidth)
+	        .height(newWidth * $el.attr('data-aspectRatio'));
+
+	  });
+
+	}).resize();
 
 });
+
+		});
--- page-landing.php
+++ page-landing.php
@@ -74,40 +74,86 @@ Template name: YOY Landing page
               <div class="modal-content">
 
                 <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal"><img src="<?php bloginfo('template_directory'); ?>/images/modal-close-icon.png"></button>
+                  <button type="button" class="close" data-dismiss="modal"><img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/modal-close-icon.png"></button>
                 </div>
 
                 <div class="modal-body">
 
                   <div class="row">
                    <div class="col-sm-6">
-                     <img src="<?php bloginfo('template_directory')?>/images/profile_alexia.png" alt="Alexia's profile" />
+                     <img class="img-responsive" src="<?php bloginfo('template_directory')?>/images/profile_alexia.png" alt="Alexia's profile" />
                    </div>
                     <div class="col-sm-6">
-                      <h2>ALEXIA L.</h2>
+                      <h2>Alexia L.</h2>
                       <h3>2016 Northeast Region<br>Youth of the Year</h3>
-                      <p>Boys &amp; Girls Clubs of Vineland</p>
-                      <p>Vineland, NJ</p>
+                      <p><b>Boys &amp; Girls Clubs of Vineland</b></p>
+                      <p><b>Vineland, NJ</b></p>
                       <p>A passionate humanitarian and powerful leader, Alexia found her voice at her Boys &amp; Girls Club. Leading 'Bags for Beauties,' she helped collect hygiene items for the homeless. Serving as president of her Club's Keystone Club, she is a champion for gun safety and violence awareness and found of a self-esteem program called 'You Can Too.' She will attend Hampton University this fall and plans to become Chief Justice of the United States.</p>
                       <h4>Cheer for Alexia!</h4>
-                      <div class="">
-                        <p>
-                          SHARE NOW TO SHOW YOUR SUPPORT
-                        </p>
+                      <div class="social-cta">
+                        <h5>SHARE NOW TO SHOW YOUR SUPPORT &nbsp;<i class="fa fa-2x fa-facebook-square" aria-hidden="true"></i> &nbsp;<i class="fa fa-2x fa-twitter-square" aria-hidden="true"></i></h5>
                       </div>
                     </div>
                   </div>
-                  <!-- <div class="row">
-                   <div class="col-sm-12">
 
+                  <div class="row">
+                   <div class="col-sm-12">
+                      <div class="videoWrapper">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/J3GIs4iLEes" frameborder="0" allowfullscreen></iframe>
+                      </div>
                    </div>
-                  </div> -->
+                  </div>
+
+                  <div class="row">
+                      <div class="col-sm-1">
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                           viewBox="0 0 107.9 84.6" enable-background="new 0 0 107.9 84.6" xml:space="preserve">
+                        <g>
+                          <g>
+                            <defs>
+                              <rect id="SVGID_125_" x="332.3" y="-1538.1" width="1440" height="1907.9"/>
+                            </defs>
+                            <clipPath id="SVGID_2_">
+                              <use xlink:href="#SVGID_125_"  overflow="visible"/>
+                            </clipPath>
+                            <g clip-path="url(#SVGID_2_)">
+                            </g>
+                          </g>
+                        </g>
+                        <g>
+                          <path fill="#76B0D8" d="M48.5,65.3c0,5.1-2.1,9.6-6.2,13.5c-4.1,3.9-9,5.8-14.7,5.8c-8.9,0-15.7-2.9-20.4-8.8C2.4,70,0,61.8,0,51.5
+                            c0-9.6,4.1-19.4,12.3-29.2c8.2-9.8,18-17.2,29.5-22.3l5.3,8.6C38,13,30.9,18.1,25.7,24c-5.2,5.9-8.1,13-9,21.3h6.8
+                            c5,0,9.1,0.6,12.2,1.7c3.1,1.1,5.7,2.7,7.6,4.7c1.9,1.9,3.2,4.1,3.9,6.4C48.1,60.4,48.5,62.8,48.5,65.3z M107.9,65.3
+                            c0,5.1-2.1,9.6-6.2,13.5c-4.1,3.9-9,5.8-14.7,5.8c-8.9,0-15.7-2.9-20.4-8.8c-4.8-5.9-7.1-14-7.1-24.3c0-9.6,4.1-19.4,12.3-29.2
+                            c8.2-9.8,18-17.2,29.5-22.3l5.3,8.6C97.4,13,90.3,18.1,85.1,24c-5.2,5.9-8.1,13-9,21.3H83c5,0,9.1,0.6,12.2,1.7
+                            c3.1,1.1,5.7,2.7,7.6,4.7c1.9,1.9,3.2,4.1,3.9,6.4C107.5,60.4,107.9,62.8,107.9,65.3z"/>
+                        </g>
+                        </svg>
+                      </div>
+                      <div class="col-sm-11">
+                        <div class="quote-wrap">
+                          <p>
+                            I no longer walk in shame, but I stride in dignity. My Club enlightened me to the facts that there are no bounds to my strength, and that I can create my own phenomenal path."
+                          </p>
+                        </div>
+                      </div>
+                  </div>
 
                 </div>
 
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-default"><a href="#modal-six" data-toggle="modal" data-dismiss="modal">Previous</a></button>
-                  <button type="button" class="btn btn-default"><a href="#modal-two" data-toggle="modal" data-dismiss="modal">Next</a></button>
+                  <section class="social">
+                    <p>Show your support for Alexia! <i class="fa fa-2x fa-facebook-square" aria-hidden="true"></i> <i class="fa fa-2x fa-twitter-square" aria-hidden="true"></i></p>
+                  </section>
+                  <section class="white-break"></section>
+                  <section class="modal-nav-container">
+                    <ul class="modal-navigation">
+                      <li><a href="#modal-six" data-toggle="modal" data-dismiss="modal"><img src="<?php bloginfo('template_directory'); ?>/images/chevron-left.png"></a></li>
+                      <li><img src="<?php bloginfo('template_directory'); ?>/images/circle_jocelyn.png" alt="Jocelyn"></li>
+                      <li><img src="<?php bloginfo('template_directory'); ?>/images/circle_raliyah.png" alt="Jocelyn"></li>
+                      <li><a href="#modal-two" data-toggle="modal" data-dismiss="modal"><img src="<?php bloginfo('template_directory'); ?>/images/chevron-right.png"></a></li>
+                    </ul>
+                  </section>
                 </div>
 
               </div>
@@ -142,6 +188,11 @@ Template name: YOY Landing page
                 </div>
                 <div class="modal-body">
                   <p>Modal Two.</p>
+                    <div class="row">
+                      <div class="col-sm-12">
+                        <iframe width="100%" height="600" src="https://www.youtube.com/embed/J3GIs4iLEes" frameborder="0" allowfullscreen></iframe>
+                      </div>
+                  </div>
                 </div>
                 <div class="modal-footer">
                   <a href="#modal-one" data-toggle="modal" data-dismiss="modal"><button type="button" class="btn btn-default">Previous</button></a>
